1f46832cccb483f29b9f583f8f1c007a9cc530fb
AMIR <55827972+amirhosseinbahrami0@users.noreply.github.com>
2024-11-21 14:59:28 +0330

    Initial commit

---
+++ ADC.c
@@ -0,0 +1,17 @@
+/*
+ * ADC.c
+ *
+ * Created: 11/21/2024 2:54:40 PM
+ *  Author: Amir
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# ADC
+ This is a Analog to Digital Convertor library for Atmega8a
