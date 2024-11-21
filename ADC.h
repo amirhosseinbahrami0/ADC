a7e669532b37986d58234f45c8772fc4a2fd2636
AMIR <55827972+amirhosseinbahrami0@users.noreply.github.com>
2024-11-21 15:47:22 +0330

    Update ADC.h

--- ADC.h
+++ ADC.h
@@ -9,8 +9,30 @@
 #ifndef ADC_H_
 #define ADC_H_
 
+/*
+*@brief Initialize the ADC
+*/
+
+void adc_init(void);
+
+/*
+*@brief get ADC value
+*@return 16-bit value of analog/digital comparison
+*/
+
+uint16_t adc_get(void);
 
 
+/*
+*@brief enable ADC for power save
+*/
 
+void adc_enable(void);
+
+/*
+* @brief disable ADC for power save
+*/
+
+void adc_disable(void);
 
 #endif /* ADC_H_ */
